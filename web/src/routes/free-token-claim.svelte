eeab1486c9fd2b4b5fe1d86bbebfab9bc1ecc51a
wighawag <wighawag@gmail.com>
2025-01-25 15:12:58 +0000

    bette rhandling of claim

--- web/src/routes/free-token-claim.svelte
+++ web/src/routes/free-token-claim.svelte
@@ -7,15 +7,39 @@
   import {privateWallet} from '$lib/account/privateWallet';
   import {myTokens} from '$lib/space/token';
   import {BigNumber, BigNumberish} from '@ethersproject/bignumber';
+  import {wait} from '$lib/utils';
 
-  async function claim(amount: number) {
-    if (wallet.address && wallet.contracts) {
-      const numTokens = BigNumber.from(amount).mul('1000000000000000000') || $myTokens.freePlayTokenClaimBalance;
-      const tx = await wallet.contracts.FreePlayTokenClaim.claim(wallet.address, numTokens);
-      // TODO :
-      //  account.recordWithdrawal(tx.hash, tx.nonce);
-    } else {
-      throw new Error(` not wallet or contracts`);
+  let error: string | undefined;
+  let waiting_tx: {balanceBefore: BigNumber; txMinted: boolean} | undefined;
+  async function claim(amount: number | undefined) {
+    try {
+      if (amount === 0) {
+        throw new Error('zero amount');
+      }
+      if (wallet.address && wallet.contracts) {
+        const numTokens = amount
+          ? BigNumber.from(amount).mul('1000000000000000000')
+          : $myTokens.freePlayTokenClaimBalance;
+
+        const balanceBefore = $myTokens.freePlayTokenClaimBalance;
+        const tx = await wallet.contracts.FreePlayTokenClaim.claim(wallet.address, numTokens);
+        waiting_tx = {balanceBefore, txMinted: false};
+        await tx.wait();
+        waiting_tx = {balanceBefore, txMinted: true};
+        while (waiting_tx) {
+          if ($myTokens.freePlayTokenClaimBalance.lt(waiting_tx.balanceBefore)) {
+            waiting_tx = undefined;
+          }
+          console.log('waiting for balance to update');
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+        }
+        // TODO :
+        //  account.recordWithdrawal(tx.hash, tx.nonce);
+      } else {
+        throw new Error(` not wallet or contracts`);
+      }
+    } catch (err: any) {
+      error = err.message || err;
     }
   }
 
@@ -38,7 +62,26 @@
           Connect
         </Button>
       {:else}
-        {#if !$myTokens.freePlayTokenClaimBalance}
+        {#if error}
+          <div class="text-center">
+            <p class="text-red-500">{error}</p>
+            <Button
+              class="block w-max-content m-4"
+              borderColor="border-red-500"
+              color="text-red-500"
+              label="ok"
+              on:click={() => (error = undefined)}
+            >
+              OK
+            </Button>
+          </div>
+        {:else if waiting_tx}
+          {#if waiting_tx.txMinted}
+            <p>Please wait the balance get updated...</p>
+          {:else}
+            <p>Please wait the tx get included...</p>
+          {/if}
+        {:else if !$myTokens.freePlayTokenClaimBalance}
           <p>Please wait...</p>
           <!-- {:else if $withdrawals.pending} -->
           <!-- <p>withdrawal in progress..</p> -->
@@ -57,8 +100,8 @@
         />
         <Button
           class="block w-max-content m-4"
-          label="withdraw"
-          disabled={!$myTokens.freePlayTokenClaimBalance || $myTokens.freePlayTokenClaimBalance.eq(0)}
+          label="claim"
+          disabled={!$myTokens.freePlayTokenClaimBalance || $myTokens.freePlayTokenClaimBalance.eq(0) || waiting_tx}
           on:click={() => claim(amount)}
         >
           claim
