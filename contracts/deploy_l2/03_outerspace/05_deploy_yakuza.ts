0cad820a2603c91ce035e7111ad7263a8ec3d69b
wighawag <wighawag@gmail.com>
2025-02-10 10:15:39 +0000

    min average yakuza planet

--- contracts/deploy_l2/03_outerspace/05_deploy_yakuza.ts
+++ contracts/deploy_l2/03_outerspace/05_deploy_yakuza.ts
@@ -22,7 +22,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     minAttackAmount: 20000,
     numSecondsPerTokens,
     spaceshipsToKeepPer10000: 1500, // 15% of cap to keep
-    minAverageStakePerPlanet: parseEther('4').toString(), // 5 tokens per planet on average minimum, do mot accept low planet unless bigger are given too
+    minAverageStakePerPlanet: parseEther('1').toString(), // 1 tokens per planet on average minimum, do mot accept low planet unless bigger are given too
     maxClaimDelay: Math.floor((1 * 24 * 60 * 60) / OuterSpace.linkedData.productionSpeedUp), // 1 day
     minimumSubscriptionWhenNotStaking: parseEther('1').toString(),
     minimumSubscriptionWhenStaking: parseEther('1').toString(),
