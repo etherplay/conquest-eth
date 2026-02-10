66e6e95cc43dd54c0cea6e5fd551e509377eaeec
wighawag <wighawag@gmail.com>
2026-02-10 09:27:40 +0000

    add name for monad and celo

--- lobsters/tools/src/tools/missiv_register.ts
+++ lobsters/tools/src/tools/missiv_register.ts
@@ -16,6 +16,10 @@ const chainNames: {[chainId: string]: string} = {
 	'31337': 'localhost chain',
 	'7001': 'ZetaChain Testnet',
 	'7000': 'ZetaChain',
+	'42220': 'celo',
+	'11142220': 'celo-sepolia',
+	'143': 'monad',
+	'10143': 'monad-testnet',
 };
 function nameForChainId(chainId: string): string {
 	const name = chainNames[chainId];
